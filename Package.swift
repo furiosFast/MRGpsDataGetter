3a5e099a19ae3a5fab47ecf809a7bdf8a7d5f834
furiosFast <furios.fast@hotmail.it>
2019-11-21 00:46:32 +0100

    fix

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "MRGpsDataGetter", dependencies: [.product(name: "Alamofire"), .product(name: "SwiftyJSON")], path: ("Sources")),
+        .target(name: "MRGpsDataGetter", dependencies: [.product(name: "Alamofire"), .product(name: "SwiftyJSON")], path: ("Source")),
     ],
     swiftLanguageVersions: [
         .v5
