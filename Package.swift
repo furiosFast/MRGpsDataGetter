df683a40f5c1d0b29160b00097d445879c259ade
furiosFast <furios.fast@hotmail.it>
2019-12-05 22:07:57 +0100

    fix

--- Package.swift
+++ Package.swift
@@ -24,17 +24,17 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "MRGpsDataGetter", targets: ["MRGpsDataGetter"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-        .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", from: "5.1.0"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3"),
+//        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+//        .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", from: "5.1.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//        .target(name: "MRGpsDataGetter", dependencies: [.product(name: "Alamofire"), .product(name: "SwiftyJSON"), .product(name: "SwifterSwift")]),
-        .target(name: "MRGpsDataGetter"),
+        .target(name: "MRGpsDataGetter", dependencies: [.product(name: "Alamofire"), .product(name: "SwiftyJSON"), .product(name: "SwifterSwift")]),
+//        .target(name: "MRGpsDataGetter"),
     ],
     swiftLanguageVersions: [
         .v5
