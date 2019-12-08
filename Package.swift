929c41c3044e2fc13746290645189e9e0c113734
furiosFast <furios.fast@hotmail.it>
2019-12-08 23:30:13 +0100

    fix readme

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     name: "MRGpsDataGetter",
     platforms: [
         // Some platform where run yours library
-        .macOS(.v10_12), .iOS(.v11), .tvOS(.v11), .watchOS(.v4)
+        .iOS(.v11), .tvOS(.v11), .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
