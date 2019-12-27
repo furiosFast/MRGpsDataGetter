be99850b4d63bbb032a7d9d1152c015ec1c8130b
furiosFast <furios.fast@hotmail.it>
2019-12-27 16:02:03 +0100

    fix

--- Source/MRGpsDataGetter/External/AstrologyCalc/EclipseCalculator.swift
+++ Source/MRGpsDataGetter/External/AstrologyCalc/EclipseCalculator.swift
@@ -142,8 +142,8 @@ public class EclipseCalculator {
                 // LUNAR ECLIPSE
                 
                 
-                e.rho = (1.2847 + u).string
-                e.sigma = (0.7494 - u).string
+                e.rho = String(format: "%3.1f", 1.2847 + u) + loc("DEGREE")
+                e.sigma = String(format: "%3.1f", 0.7494 - u) + loc("DEGREE")
                 
                 // Phase for umbral eclipse
                 // AFFC, p. 135, f. 33.4
