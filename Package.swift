81a1bd7cd4c5e318a63c392520814766851cdafd
furiosFast <furios.fast@hotmail.it>
2020-10-23 00:18:22 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "MRGpsDataGetter",
                 dependencies: [.product(name: "Alamofire"), .product(name: "SwiftyJSON"), .product(name: "SwifterSwift"), .product(name: "EKAstrologyCalc")],
-                resources: [.process("Resources/Localizations")]),
+                resources: [.process("Resources")]),
 //        .target(name: "MRGpsDataGetter"),
     ],
     swiftLanguageVersions: [
