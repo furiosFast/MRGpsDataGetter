5400edf49ad3e456e423c4f91f83f70725692b72
furiosFast <furios.fast@hotmail.it>
2021-02-04 23:39:04 +0100

    test

---
+++ Source/MRGpsDataGetter/ddddd.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marco Ricca on 04/02/2021
+//
+//  Created for  in 04/02/2021
+//  Using Swift 5.0
+//  Running on macOS 10.15
+//
+//  
+//
+		
+
+import Foundation
